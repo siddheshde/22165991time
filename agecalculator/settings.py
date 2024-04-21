09bbf1742b847edf21a7213128c94870e1964058
Siddhesh Deshmukh <siddheshdeshmukh1234@gmail.com>
2024-04-21 17:50:53 +0100

    edit commit

--- agecalculator/settings.py
+++ agecalculator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cvgm$@w(e4^f8g+ci-0q4j(d5eaenps^l&6tc2u)^)t!#1zq77
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
