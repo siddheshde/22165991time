6518a0cea48170cf96b7182e8d2317e586dbc62c
Siddhesh Deshmukh <siddheshdeshmukh1234@gmail.com>
2024-04-21 21:40:07 +0100

    final commit

--- agecalculator/urls.py
+++ agecalculator/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('calculate/', include(('calculator.urls', 'calculator'), namespace='calculator')),
+    path('', include(('calculator.urls', 'calculator'), namespace='calculator')),
     path('calculator/', include('calculator.urls')),
 ]
